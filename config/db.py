ca0d8b65401373be80201f527f23c3eb1ca61a1f
hustchihieu143 <chihieusky@gmail.com>
2022-10-06 23:28:07 +0700

    fix hard code config

--- config/db.py
+++ config/db.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine, MetaData
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-db_engine = create_engine("mysql+pymysql://test:1@localhost:3100/mail_10_minutes")
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+db_config = port = os.environ.get("MYSQL_DATABASE_URL")
+
+db_engine = create_engine(db_config)
 meta = MetaData()
 conn = db_engine.connect()
 
